8856299e8795e21d0ca1ccce307aae8b7fbcae98
Undying-Merch <jonasnikolailj@gmail.com>
2023-02-08 14:09:04 +0100

    New Skeleton
    Made a new Skeleton for the APP

---
+++ app/src/main/java/com/example/diktatorapp/SearchForPhone.java
@@ -0,0 +1,14 @@
+package com.example.diktatorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchForPhone extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_for_phone);
+    }
+}
