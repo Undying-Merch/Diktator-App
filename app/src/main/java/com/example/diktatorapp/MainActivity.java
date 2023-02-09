41d3c8b4b3a3885e6de49a27241c14f8538a092e
Undying-Merch <jonasnikolailj@gmail.com>
2023-02-09 09:06:49 +0100

    Cleaning Computer
    Changing computer, so making a commit for small changes.

--- app/src/main/java/com/example/diktatorapp/MainActivity.java
+++ app/src/main/java/com/example/diktatorapp/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.diktatorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String userName = "";
+    private String pass = "";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void logon(View view){
+        EditText userText = (EditText) findViewById(R.id.loginUserName);
+        EditText passText = (EditText) findViewById(R.id.loginPassword);
+        TextView testText = (TextView) findViewById(R.id.loginTest);
+        userName = userText.getText().toString();
+        pass = passText.getText().toString();
+
+
     }
 }
